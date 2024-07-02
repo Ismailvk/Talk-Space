b2c3a08e824691f60c353a0362a9379e5ae9a8c6
Ismailvk <ismailkbassi00@gmail.com>
2024-07-02 17:48:25 +0530

    updated

--- lib/controller/login/login_bloc.dart
+++ lib/controller/login/login_bloc.dart
@@ -26,6 +26,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
 
       if (user != null) {
         print('Login Successfull the user :$user');
+        print('success');
         Navigator.of(event.context).pushAndRemoveUntil(
             MaterialPageRoute(
                 builder: (context) => HomeScreen(user1: user.displayName!)),
