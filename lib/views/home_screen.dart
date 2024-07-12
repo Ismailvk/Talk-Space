d7c72e403c96b1ed3828878c7817534f014fff71
Ismailvk <ismailkbassi00@gmail.com>
2024-07-12 18:30:17 +0530

    updated

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
       //online logics
       setStatus('Online');
     } else {
-      //offline
+      //offline logics
       setStatus('Offline');
     }
   }
